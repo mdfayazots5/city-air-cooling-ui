05e8457c0b2005db350945a0db727363d1757bbd
Mohammed Fayaz <124465732+mdfayazots5@users.noreply.github.com>
2026-03-21 07:09:12 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://city-air-cooling-api.onrender.com',
+  apiUrl: 'https://city-air-cooling-api.onrender.com/api/',
   siteUrl: 'https://www.coolzo.in'
 };
